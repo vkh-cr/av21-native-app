46ffffb98a73e0add56b5ddbf58ebb7145099185
Jakub Simandl <simandl.jkb@gmail.com>
2021-07-13 11:20:48 +0200

    Fix styles but actually break them

--- src/screens/Activity.tsx
+++ src/screens/Activity.tsx
@@ -3,13 +3,21 @@ import { DrawerScreenProps } from "@react-navigation/drawer";
 
 import { View, ScrollView, Image, Text } from "react-native";
 import { AppHeader } from "../components/AppHeader";
-import { Avatar, Chip, Divider, Headline, Paragraph, Surface, Title } from "react-native-paper";
+import {
+  Avatar,
+  Chip,
+  Divider,
+  Headline,
+  Paragraph,
+  Surface,
+  Title,
+} from "react-native-paper";
 import { StyleSheet } from "react-native";
 import { useActivity } from "../hooks/useActivity";
-import default_logo from "../../assets/activity_default.png"
+import default_logo from "../../assets/activity_default.png";
 import ActivityInfoPanel from "../components/ActivityInfo";
 interface ActivityProps
-  extends DrawerScreenProps<Record<string, object | undefined>, "Activity"> { }
+  extends DrawerScreenProps<Record<string, object | undefined>, "Activity"> {}
 
 export default function Activity({ navigation, route }: ActivityProps) {
   const params = route.params as { activityId: string };
@@ -22,49 +30,65 @@ export default function Activity({ navigation, route }: ActivityProps) {
         title={activity?.title || "Aktivita"}
         // subtitle="nevim"
         navigation={navigation}
-        removeShadow
+        removeShadow={activity?.image ? false : true}
       />
       <View>
         <Image source={default_logo} style={styles.image} />
-        <ScrollView contentContainerStyle={styles.scrollView}>
-          <Headline>{activity?.title || ""}</Headline>
+        <View style={styles.bottomView}>
+          <View style={styles.headlineContainer}>
+            <Headline style={styles.headline}>{activity?.title || ""}</Headline>
+          </View>
           <ActivityInfoPanel icon="calendar" text={`${activity?.date}`} />
           <ActivityInfoPanel icon="map" text={`${activity?.location}`} />
-          <ActivityInfoPanel icon="account" text={`${activity?.presenter.firstName} ${activity?.presenter.lastName}`} />
+          <ActivityInfoPanel
+            icon="account"
+            text={`${activity?.presenter.firstName} ${activity?.presenter.lastName}`}
+          />
           <Divider />
           <Title>Anotace</Title>
-          <Paragraph>
-            {activity?.description}
-          </Paragraph>
-        </ScrollView>
+          <View style={styles.scrollViewContainer}>
+            <ScrollView contentContainerStyle={styles.scrollView}>
+              <Paragraph>{activity?.description}</Paragraph>
+            </ScrollView>
+          </View>
+        </View>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollViewContainer: { flex: 1 },
+  scrollView: {},
   image: {
     width: "100%",
-    height: 350,
+    height: "25%",
     backgroundColor: "#056970",
     marginBottom: -125,
-    zIndex: 1,
+    zIndex: 2,
   },
-  scrollView: {
+  bottomView: {
     height: "100%",
     marginTop: 0,
-    paddingTop: 10,
+    paddingTop: 25,
     paddingBottom: 20,
     paddingLeft: 30,
     paddingRight: 30,
-    // alignItems: "center",
     borderTopRightRadius: 60,
     borderTopLeftRadius: 60,
     backgroundColor: "#FFF",
-    zIndex: 2,
+    zIndex: 3,
+  },
+  headlineContainer: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
   },
   headline: {
     marginTop: 20,
+    fontSize: 26,
+    fontWeight: "600",
   },
   paragraph: {
     marginTop: 20,
